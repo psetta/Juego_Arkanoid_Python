c9268e3a00c55fb16ba4ad3ac03ffc8f05b70cc7
dawMP - Mayo Lado <A13ManuelML@sanclemente.local>
2019-02-26 20:31:27 +0100

    working

--- python_arkanoid.py
+++ python_arkanoid.py
@@ -1,30 +1,63 @@
-# -*- coding: utf-8 -*-
-
 import pygame
 import pygame.gfxdraw
 from pygame.locals import *
 import math
 
+_alto_ventana = 500
+_ancho_ventana = 500
+_marco = 5
+_ancho_paleta = 50
+_alto_paleta = 15
+_radio_bola = 6
+_ancho_bloque = 20
+_alto_bloque = 10
+
 class Punto:
 	def __init__(self,x,y):
 		self.x = x
 		self.y = y
+		
 	def __add__(self,other):
 		return punto(self.x+other.x,self.y+other.y)
+		
+	def __len__(self):
+		return 2
+		
+	def __getitem__(self,item):
+		if item == 0:
+			return self.x
+		elif item == 1:
+			return self.y
+		else:
+			raise IndexError()
+			
+class Ventana:
+	def __init__(self,ancho,alto,marco,score_alto):
+		self.ancho = ancho
+		self.alto = alto
+		self.marco = marco
+		self.score_alto = score_alto
+		self.pygame = pygame.display.set_mode([ancho,alto],pygame.RESIZABLE)
+		self.rect_pygame = pygame.Rect(marco,marco+score_alto,
+									   ancho-(marco*2),alto-(marco+score_alto))
+		self.rect_pygame_score = pygame.Rect(marco,
+											 marco,
+											 ancho-(marco*2),
+											 score_alto-marco)
 		
 class Bola:
 	velocidad_total = 6
 	velocidad_max_x = 5
 	velocidad_x = 0
 	velocidad_y = -velocidad_total
+	pegada = True
 	def __init__(self,x,y,radio):
-		self.x = x
-		self.y = y
+		self.punto = Punto(x,y)
 		self.radio = radio
 		self.diametro = self.radio*2
 		
 	def update_vel_x(self,paleta):
-		choque = self.x+self.radio - paleta.x
+		choque = self.punto.x+self.radio - paleta.punto.x
 		self.velocidad_x = ((((choque * 100)/ANCHO_PALETA_TOTAL) * 0.1)
 							- self.velocidad_max_x)
 		self.velocidad_x = max(-self.velocidad_max_x, self.velocidad_x)
@@ -34,94 +67,147 @@ class Bola:
 		self.velocidad_y = (self.velocidad_total * (math.sin(
 			math.radians(90 - math.degrees(math.asin(self.velocidad_x/float(
 			self.velocidad_total)))))))
-	
-class Ventana:
-	def __init__(self,ancho,alto,marco):
-		self.ancho = ancho
-		self.alto = alto
-		self.marco = marco
 		
 class Paleta:
-	def __init__(self,x,y,ancho,alto):
+	def __init__(self,x,y,ancho,alto,velocidad):
+		self.punto = Punto(x,y)
 		self.ancho = ancho
 		self.alto = alto
+		self.velocidad = velocidad
+		self.rect_pygame = pygame.Rect(x,y,ancho,alto)
 		
 class Bloque:
-	def __init__(self,ancho,alto):
+	def __init__(self,x,y,ancho,alto):
+		self.punto = Punto(x,y)
 		self.ancho = ancho
 		self.alto = alto
-
-ventana = Ventana(500,400,5)
-paleta = Paleta(50,10)
-bola = Bola(0,0,5)
-
 		
-punto_paleta = punto((ANCHO_VENTANA/2-ANCHO_PALETA/2),ALTO_VENTANA-ALTO_PALETA*2)
-
-punto_bola = punto((punto_paleta.x+ANCHO_PALETA/2-LADO_BOLA/2),(punto_paleta.y-LADO_BOLA))
-
-
-pygame.init()
-
+class Status:
+	def __init__(self):
+		self.vidas = 5
+		self.score = 0
+		self.game_over = 0
+		self.win = 0
+		self.pygame_bucle = 1
+	
+class Game:
+	def __init__(self):
+		pygame.init()
+		self.status = Status()
+		self.ventana = Ventana(_ancho_ventana,_alto_ventana,_marco,
+							   _alto_ventana/15)			   
+		self.paleta = Paleta(int(self.ventana.ancho/2-_ancho_paleta/2),
+							 int(self.ventana.alto-_alto_paleta*2),
+							 _ancho_paleta,
+							 _alto_paleta,
+							 5)					
+		self.bola = Bola(int(self.paleta.punto.x+self.paleta.ancho/2),
+						 int(self.paleta.punto.y-_radio_bola),
+						 _radio_bola)
+						 
+		self.font = pygame.font.SysFont("System", int(self.ventana.ancho/20))
+		
+	def dibujar_relleno(self):
+		if self.status.game_over:
+			color = [200,0,0]
+		elif self.status.win:
+			color = [0,200,0]
+		else:
+			color = [15,15,15]
+		self.ventana.pygame.fill(color)
+		
+	def dibujar_fondo(self):
+		color = [250,250,250]
+		pygame.draw.rect(self.ventana.pygame,color,self.ventana.rect_pygame)
+		color = [230,230,230]
+		pygame.draw.rect(self.ventana.pygame,color,self.ventana.rect_pygame_score)
+		
+	def dibujar_paleta(self):
+		color = [150,150,150]
+		pygame.draw.rect(self.ventana.pygame,color,self.paleta.rect_pygame)
+		
+	def dibujar_bola(self):
+		color = [20,150,200]
+		pygame.gfxdraw.filled_circle(self.ventana.pygame,
+									 self.bola.punto.x,
+									 self.bola.punto.y,
+									 self.bola.radio,
+									 color)
+		color = [20,150,200]
+		pygame.gfxdraw.aacircle(self.ventana.pygame,
+								self.bola.punto.x,
+								self.bola.punto.y,
+								self.bola.radio,
+								color)
+								
+	def dibujar_score(self):
+		color = [15,15,15]
+		text_score = self.font.render(("SCORE: "+str(self.status.score)),1,color)
+		self.ventana.pygame.blit(text_score,[self.ventana.marco,self.ventana.marco])
+		
+	def dibujar_vidas(self):
+		color = [15,15,15]
+		text_vidas = self.font.render(("VIDAS: "+str(self.status.vidas)),1,color)
+		self.ventana.pygame.blit(text_vidas,[self.ventana.ancho-
+								(self.ventana.marco+text_vidas.get_width()),
+								 self.ventana.marco])
+		
+	def dibujar_todo(self):
+		self.dibujar_relleno()
+		self.dibujar_fondo()
+		self.dibujar_paleta()
+		self.dibujar_bola()
+		self.dibujar_score()
+		self.dibujar_vidas()
+		
+	def tecla_pulsada(self):
+		return pygame.key.get_pressed()
+		
+	def movimiento(self):
+		if self.tecla_pulsada[K_LEFT]:
+			self.paleta.punto.x -= paleta.velocidad
+		
+	if tecla_pulsada[K_LEFT]:
+		punto_paleta.x -= VELOCIDADE_PALETA
+	if tecla_pulsada[K_RIGHT]:
+		punto_paleta.x += VELOCIDADE_PALETA
+		
+	def start(self):
+		while self.status.pygame_bucle:
+			reloj = pygame.time.Clock()
+			
+			self.dibujar_todo()
+			
+			pygame.display.update()
+			
+			for evento in pygame.event.get():
+				if evento.type == pygame.QUIT:
+					pygame.display.quit()
+					self.status.pygame_bucle = 0
+			reloj.tick(60)
+		
+		print("Bye!")
+			
+			
+"""
 #CREAR LISTA DE BLOQUES
 
-lista_bloques = []
+#lista_bloques = []
 
-def crear_lista_bloques():
-	for i in range(MARCO,ANCHO_VENTANA-MARCO,ANCHO_BLOQUE):
-		lista_bloques.append(pygame.Rect(i,ALTO_VENTANA/5,ANCHO_BLOQUE,ALTO_BLOQUE))
-	for i in range(MARCO,ANCHO_VENTANA-MARCO,ANCHO_BLOQUE):
-		lista_bloques.append(pygame.Rect(i,ALTO_VENTANA/5+ALTO_BLOQUE,ANCHO_BLOQUE,ALTO_BLOQUE))
-	for i in range(MARCO,ANCHO_VENTANA-MARCO,ANCHO_BLOQUE):
-		lista_bloques.append(pygame.Rect(i,ALTO_VENTANA/5+ALTO_BLOQUE*2,ANCHO_BLOQUE,ALTO_BLOQUE))
-	for i in range(MARCO,ANCHO_VENTANA-MARCO,ANCHO_BLOQUE):
-		lista_bloques.append(pygame.Rect(i,ALTO_VENTANA/5+ALTO_BLOQUE*3,ANCHO_BLOQUE,ALTO_BLOQUE))
-	for i in range(MARCO,ANCHO_VENTANA-MARCO,ANCHO_BLOQUE):
-		lista_bloques.append(pygame.Rect(i,ALTO_VENTANA/5+ALTO_BLOQUE*4,ANCHO_BLOQUE,ALTO_BLOQUE))
-	for i in range(MARCO,ANCHO_VENTANA-MARCO,ANCHO_BLOQUE):
-		lista_bloques.append(pygame.Rect(i,ALTO_VENTANA/5+ALTO_BLOQUE*5,ANCHO_BLOQUE,ALTO_BLOQUE))
-	for i in range(MARCO,ANCHO_VENTANA-MARCO,ANCHO_BLOQUE):
-		lista_bloques.append(pygame.Rect(i,ALTO_VENTANA/5+ALTO_BLOQUE*6,ANCHO_BLOQUE,ALTO_BLOQUE))
-	for i in range(MARCO,ANCHO_VENTANA-MARCO,ANCHO_BLOQUE):
-		lista_bloques.append(pygame.Rect(i,ALTO_VENTANA/5+ALTO_BLOQUE*8,ANCHO_BLOQUE,ALTO_BLOQUE))
+#def crear_lista_bloques():
+#	for i in range(ventana.marco,ventana.ancho-ventana.marco,_ancho_bloque):
+#		lista_bloques.append(pygame.Rect(i,ALTO_VENTANA/5,ANCHO_BLOQUE,ALTO_BLOQUE))
 
-crear_lista_bloques()
+#crear_lista_bloques()
 
-ventana = pygame.display.set_mode([ANCHO_VENTANA, ALTO_VENTANA])
-
-font = pygame.font.SysFont("System", ANCHO_VENTANA/20)
-
-pelota_pegada = True
-
-vidas = 5
-
-score = 0
-
-game_over = False
-victoria = False
-
-ON = True
-
-#BUCLE DO XOGO
-
-while ON:
+#BUCLE
+while 0:
 
 	reloj = pygame.time.Clock()
 	
-	#DEBUXADO:
-	
-	if game_over:
-		ventana.fill([200,0,0])
-	elif victoria:
-		ventana.fill([0,200,0])
-	else:
-		ventana.fill([20,20,20])
-	
-	rect_xogo = pygame.Rect(MARCO, MARCO, ANCHO_VENTANA-(MARCO*2), ALTO_VENTANA-MARCO)
-	
 	#DEBUXADO PALETA
 	
-	pygame.draw.rect(ventana, [250,250,250], rect_xogo)
+	pygame.draw.rect(ventana, [250,250,250], ventana.rect_pygame)
 	
 	pygame.gfxdraw.aacircle(ventana, punto_paleta.x, punto_paleta.y+ALTO_PALETA/2, RADIO_CIRCULO, [0,0,0])
 	pygame.gfxdraw.filled_circle(ventana, punto_paleta.x, punto_paleta.y+RADIO_CIRCULO, ALTO_PALETA/2, [200,0,0])
@@ -270,3 +356,9 @@ while ON:
 			ON = False
 			
 	reloj.tick(60)
+	
+	"""
+	
+if __name__ == "__main__":	
+	game = Game()
+	game.start()
